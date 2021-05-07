1e453484424477c33976a375d87f26366edd3e46
HuanChiang <puppy1607@gmail.com>
2021-05-08 02:04:59 +0800

    pointer

--- pointer.c
+++ pointer.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(void) {
+	char *name1[3] = {"Joey", "Cindy", "Paul"};
+	char *name2 = "Monkey";
+	char name3[5] = {'J', 'O', 'E', 'Y'};
+
+	for (int i=0; i<3; i++) 
+	{
+		printf("%s\n", name1[i]);	
+	}
+
+
+	for(int i=0;i<3;i++)
+	{
+		int j=0;
+		while (name1[i][j] != '\0') 
+		{
+			printf("%c", name1[i][j]);
+			j++;
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
